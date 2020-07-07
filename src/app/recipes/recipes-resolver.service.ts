89a1b8a9fb82836b968c36104bda630af449a899
Fahad Qayyum <fahadq@my.yorku.ca>
2020-07-06 23:42:33 -0400

    Feat
    Add Http Routing and resolver

---
+++ src/app/recipes/recipes-resolver.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
+import {Recipe} from "./recipe.model";
+import {Observable} from "rxjs";
+import {DataStorageService} from "../shared/data-storage.service";
+import {RecipeService} from "../shared/recipe.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesResolverService implements Resolve<Recipe[]> {
+
+  constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) {
+  }
+
+  // we are not subscribing here because resolve subscribe automatically to see once the data is there
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Recipe[]> | Promise<Recipe[]> | Recipe[] {
+    const recipes = this.recipeService.getRecipes();
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
+    }
+  }
+
+}
