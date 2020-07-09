4105a41f05623ba28188f82152168a7ba5954d5d
Fahad Qayyum <fahadq@my.yorku.ca>
2020-07-08 23:48:51 -0400

    Feat
    Adding layer of sercuirty using token exchange

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {RecipeService} from "./recipe.service";
+import {Recipe} from "../recipes/recipe.model";
+import {map, tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  }
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+
+    // put requests overwrite all the data that is there
+    // you can return the request which is observable here and subscribe to it in the component if you are interested in the response
+    this.http.put('https://ng-recipe-book-f2d73.firebaseio.com/recipes.json', recipes).subscribe((response) => {
+      console.log(response);
+    });
+  }
+
+  fetchRecipes() {
+    return this.http.get<Recipe[]>('https://ng-recipe-book-f2d73.firebaseio.com/recipes.json').pipe(map((recipes) => {
+      return recipes.map(recipe => {
+        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+      });
+    }), tap(recipes => {
+      this.recipeService.setRecipes(recipes);
+    }))
+  }
 }
--- src/app/shared/recipe.service.ts
+++ src/app/shared/recipe.service.ts
@@ -11,16 +11,18 @@ export class RecipeService {
 
   recipeChanged = new Subject<Recipe[]>();
 
-  recipes: Recipe[] = [
-    new Recipe(1, 'Fish Salmon',
-      'Its taste great and is not that spicy but very healthy',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-      [new Ingredient('banana', 4), new Ingredient('apple', 5)]),
-    new Recipe(2, 'Salad',
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dolore eligendi enim fuga hic ipsa nostrum perferendis praesentium repudiandae sit? Dolores fugit inventore modi nostrum quas quibusdam ullam voluptas voluptates.',
-      'https://feelgoodfoodie.net/wp-content/uploads/2019/02/Mediterranean-Chopped-Salad-5-500x375.jpg',
-      [new Ingredient('cabbage', 4), new Ingredient('leafs', 50)])
-  ];
+  // recipes: Recipe[] = [
+  //   new Recipe('Fish Salmon',
+  //     'Its taste great and is not that spicy but very healthy',
+  //     'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+  //     [new Ingredient('banana', 4), new Ingredient('apple', 5)]),
+  //   new Recipe('Salad',
+  //     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dolore eligendi enim fuga hic ipsa nostrum perferendis praesentium repudiandae sit? Dolores fugit inventore modi nostrum quas quibusdam ullam voluptas voluptates.',
+  //     'https://feelgoodfoodie.net/wp-content/uploads/2019/02/Mediterranean-Chopped-Salad-5-500x375.jpg',
+  //     [new Ingredient('cabbage', 4), new Ingredient('leafs', 50)])
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {
   }
@@ -39,7 +41,7 @@ export class RecipeService {
   }
 
   findRecipeById(id: number): Recipe {
-    return this.recipes[id - 1];
+    return this.recipes[id];
   }
 
   addRecipe(recipe: Recipe) {
@@ -54,6 +56,11 @@ export class RecipeService {
 
   deleteRecipe(index: number) {
     this.recipes.splice(index - 1, 1);
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
     this.recipeChanged.next(this.recipes.slice());
   }
 }
