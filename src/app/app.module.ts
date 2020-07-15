07b95051819357f51b43c13577cfb79940b4c0f3
Fahad Qayyum <fahadq@my.yorku.ca>
2020-07-14 20:53:56 -0400

    Feat
    Adding Angular Universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import {CoreModule} from "./core.module";
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
     SharedModule,
     CoreModule,
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable, PLATFORM_ID} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {BehaviorSubject, Observable, throwError} from "rxjs";
 import {catchError, tap} from "rxjs/operators";
 import {User} from "./user.model";
 import {Router} from "@angular/router";
 import {environment} from "../../environments/environment";
+import {isPlatformBrowser} from "@angular/common";
 
 
 export interface AuthResponseData {
@@ -25,7 +26,8 @@ export class AuthService {
   user = new BehaviorSubject<User>(null);
   private tokenExpirationTimer: any;
 
-  constructor(private http: HttpClient, private router: Router) {
+  // PLATFORM_ID is provided by angular which tells you on which platform you are running the application
+  constructor(private http: HttpClient, private router: Router, @Inject(PLATFORM_ID) private platformId) {
   }
 
   signUp(email: string, password: string): Observable<any> {
@@ -62,7 +64,10 @@ export class AuthService {
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
-    localStorage.removeItem('userData')
+
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem('userData')
+    }
     if (this.tokenExpirationTimer) {
       clearTimeout(this.tokenExpirationTimer);
     }
@@ -70,22 +75,25 @@ export class AuthService {
   }
 
   autoLogin() {
-    const userData: {
-      email: string,
-      id: string,
-      _token: string,
-      _tokenExpirationDate: string
-    } = JSON.parse(localStorage.getItem('userData'));
-    if (!userData) {
-      return;
-    } else {
-      const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
-      if (loadedUser.token) {
-        this.user.next(loadedUser);
-        const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
-        this.autoLogout(expirationDuration)
+    if (isPlatformBrowser(this.platformId)) {
+      const userData: {
+        email: string,
+        id: string,
+        _token: string,
+        _tokenExpirationDate: string
+      } = JSON.parse(localStorage.getItem('userData'));
+      if (!userData) {
+        return;
+      } else {
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+        if (loadedUser.token) {
+          this.user.next(loadedUser);
+          const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+          this.autoLogout(expirationDuration)
+        }
       }
     }
+
   }
 
   autoLogout(expirationDuration: number) {
